cfbcef45133b79946b045fa481d8ef7125bd4f15
周睿 <zrufo747@outlook.com>
2025-04-28 09:42:07 +0800

    update

--- interface/rdif-serial/src/lib.rs
+++ interface/rdif-serial/src/lib.rs
@@ -1,3 +1,22 @@
 #![no_std]
 
 extern crate alloc;
+
+use alloc::boxed::Box;
+
+use rdif_base::DriverGeneric;
+
+pub type Error = embedded_hal_nb::serial::ErrorKind;
+pub trait Sender: Send {
+    fn can_write(&self) -> bool;
+    fn write(&mut self, byte: u8) -> Result<(), Error>;
+}
+
+pub trait Serial: DriverGeneric {
+    fn split(&mut self) -> Option<(Box<dyn Sender>, Box<dyn Receiver>)>;
+}
+
+pub trait Receiver: Send {
+    fn can_read(&self) -> bool;
+    fn read(&mut self) -> Result<u8, Error>;
+}
