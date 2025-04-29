d6af12e6b968d27a2e196a6589386954ed15490b
周睿 <zrufo747@outlook.com>
2025-04-29 14:54:21 +0800

    优化intc api

--- interface/rdif-intc/src/lib.rs
+++ interface/rdif-intc/src/lib.rs
@@ -11,25 +11,32 @@ pub use rdif_base::{DriverGeneric, DriverResult, IrqConfig, IrqId, Trigger};
 custom_type!(CpuId, usize, "{:#x}");
 
 pub type Hardware = Box<dyn Interface>;
-pub type HardwareCPU = Box<dyn InterfaceCPU>;
 
 /// Fdt 解析 `interrupts` 函数，一次解析一个`cell`
-pub type FdtParseConfigFn =
+pub type FuncFdtParseConfig =
     fn(prop_interrupts_one_cell: &[u32]) -> Result<IrqConfig, Box<dyn Error>>;
 
-pub struct FdtProbeInfo {
-    pub hardware: Hardware,
-    pub fdt_parse_config_fn: FdtParseConfigFn,
+/// 在中断中调用，不会被打断，视为`Sync`
+pub trait InterfaceCPUNormal: Send + Sync {
+    fn ack(&self) -> Option<IrqId>;
+    fn eoi(&self, irq: IrqId);
 }
 
-/// 在中断中调用，不会被打断，视为`Sync`
-pub trait InterfaceCPU: Send + Sync {
-    fn get_and_acknowledge_interrupt(&self) -> Option<IrqId>;
-    fn end_interrupt(&self, irq: IrqId);
+pub trait InterfaceCPUEoiTwoStep: Send + Sync {
+    fn ack(&self) -> Option<IrqId>;
+    /// 降级优先级，允许中断被抢占
+    fn priority_drop(&self, intid: IrqId);
+    /// 中断处理完成，可继续触发中断
+    fn deactivation(&self, intid: IrqId);
+}
+
+pub enum HardwareCPU {
+    Normal(Box<dyn InterfaceCPUNormal>),
+    EoiTwoStep(Box<dyn InterfaceCPUEoiTwoStep>),
 }
 
 pub trait Interface: DriverGeneric {
-    fn current_cpu_setup(&self) -> HardwareCPU;
+    fn cpu_interface(&self) -> HardwareCPU;
     fn irq_enable(&mut self, irq: IrqId);
     fn irq_disable(&mut self, irq: IrqId);
     fn set_priority(&mut self, irq: IrqId, priority: usize);
@@ -41,5 +48,5 @@ pub trait Interface: DriverGeneric {
 }
 
 pub enum Capability {
-    FdtParseConfigFn(FdtParseConfigFn),
+    FdtParseConfig(FuncFdtParseConfig),
 }
