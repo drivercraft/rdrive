fc11bb54794825618bd8f9dd59f2d6df8a72d510
周睿 <zrufo747@outlook.com>
2025-05-14 09:47:48 +0800

    intc api

--- interface/rdif-intc/src/lib.rs
+++ interface/rdif-intc/src/lib.rs
@@ -3,7 +3,7 @@
 extern crate alloc;
 
 pub use alloc::{boxed::Box, vec::Vec};
-use core::error::Error;
+use core::{error::Error, fmt::Display};
 
 use cfg_if::cfg_if;
 use rdif_base::custom_type;
@@ -18,33 +18,33 @@ pub type HardwareCPU = Box<dyn InterfaceCPU>;
 pub type FuncFdtParseConfig =
     fn(prop_interrupts_one_cell: &[u32]) -> Result<IrqConfig, Box<dyn Error>>;
 
-cfg_if! {
-    if #[cfg(target_arch = "aarch64")]{
-        pub trait InterfaceCPU: Send + Sync {
-            fn setup(&self);
+pub trait InterfaceCPU: Send + Sync {
+    fn setup(&self);
+    fn ack(&self) -> Option<IrqId>;
+    fn eoi(&self, irq: IrqId);
+
+    cfg_if! {
+        if #[cfg(target_arch = "aarch64")]{
+            fn dir(&self, intid: IrqId);
             fn set_eoi_mode(&self, b: bool);
             fn get_eoi_mode(&self) -> bool;
-            fn ack(&self) -> Option<IrqId>;
-            fn eoi(&self, intid: IrqId);
-            fn dir(&self, intid: IrqId);
-        }
-    }else{
-        /// 在中断中调用，不会被打断，视为`Sync`
-        pub trait InterfaceCPU: Send + Sync {
-            fn setup(&self);
-            fn ack(&self) -> Option<IrqId>;
-            fn eoi(&self, irq: IrqId);
         }
     }
+
+    fn irq_enable(&self, irq: IrqId) -> Result<(), IntcError>;
+    fn irq_disable(&self, irq: IrqId) -> Result<(), IntcError>;
+    fn set_priority(&self, irq: IrqId, priority: usize) -> Result<(), IntcError>;
+    fn set_trigger(&self, irq: IrqId, trigger: Trigger) -> Result<(), IntcError>;
 }
 
 pub trait Interface: DriverGeneric {
     fn cpu_interface(&self) -> HardwareCPU;
-    fn irq_enable(&mut self, irq: IrqId);
-    fn irq_disable(&mut self, irq: IrqId);
-    fn set_priority(&mut self, irq: IrqId, priority: usize);
-    fn set_trigger(&mut self, irq: IrqId, trigger: Trigger);
-    fn set_target_cpu(&mut self, irq: IrqId, cpu: CpuId);
+
+    fn irq_enable(&mut self, irq: IrqId) -> Result<(), IntcError>;
+    fn irq_disable(&mut self, irq: IrqId) -> Result<(), IntcError>;
+    fn set_priority(&mut self, irq: IrqId, priority: usize) -> Result<(), IntcError>;
+    fn set_trigger(&mut self, irq: IrqId, trigger: Trigger) -> Result<(), IntcError>;
+    fn set_target_cpu(&mut self, irq: IrqId, cpu: CpuId) -> Result<(), IntcError>;
     fn capabilities(&self) -> Vec<Capability> {
         Vec::new()
     }
@@ -53,3 +53,15 @@ pub trait Interface: DriverGeneric {
 pub enum Capability {
     FdtParseConfig(FuncFdtParseConfig),
 }
+
+#[derive(Debug, Clone, Copy)]
+pub enum IntcError {
+    IrqIdNotCompatible { id: IrqId },
+    NotImplemented,
+}
+impl Display for IntcError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+impl Error for IntcError {}
