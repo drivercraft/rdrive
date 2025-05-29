7ad8e1179f53c5b0147a54cc0b8dbc4d207711eb
周睿 <zrufo747@outlook.com>
2025-05-29 14:07:02 +0800

    intc

--- interface/rdif-intc/src/lib.rs
+++ interface/rdif-intc/src/lib.rs
@@ -2,73 +2,78 @@
 
 extern crate alloc;
 
-pub use alloc::{boxed::Box, vec::Vec};
-use core::{error::Error, fmt::Display};
+use alloc::boxed::Box;
+use core::error::Error;
 
-use cfg_if::cfg_if;
 pub use rdif_base::{CpuId, DriverGeneric, KError, irq::*};
 
-pub type Hardware = Box<dyn Interface>;
-pub type BoxCPU = Box<dyn InterfaceCPU>;
-pub type BoxCPUCapLocalIrq = Box<dyn CPUCapLocalIrq>;
+/// CPU local interrupt controller interface
+pub mod local {
+    use super::*;
+
+    /// Boxed interface
+    pub type Boxed = Box<dyn Interface>;
+
+    pub trait Interface: DriverGeneric + Sync {
+        fn ack(&self) -> Option<IrqId>;
+        fn eoi(&self, irq: IrqId);
+
+        cfg_if::cfg_if! {
+            if #[cfg(target_arch = "aarch64")]{
+                fn dir(&self, intid: IrqId);
+                fn set_eoi_mode(&self, b: bool);
+                fn get_eoi_mode(&self) -> bool;
+            }
+        }
+
+        fn capability(&self) -> Capability;
+    }
+
+    /// controller capability
+    pub enum Capability {
+        None,
+        LocalIrq(cap::BoxedLocalIrq),
+    }
+
+    pub mod cap {
+        use super::*;
+
+        pub type BoxedLocalIrq = Box<dyn LocalIrq>;
+
+        pub trait LocalIrq: Send + Sync {
+            fn irq_enable(&self, irq: IrqId) -> Result<(), IntcError>;
+            fn irq_disable(&self, irq: IrqId) -> Result<(), IntcError>;
+            fn set_priority(&self, irq: IrqId, priority: usize) -> Result<(), IntcError>;
+            fn set_trigger(&self, irq: IrqId, trigger: Trigger) -> Result<(), IntcError>;
+        }
+    }
+}
 
 /// Fdt 解析 `interrupts` 函数，一次解析一个`cell`
 pub type FuncFdtParseConfig =
     fn(prop_interrupts_one_cell: &[u32]) -> Result<IrqConfig, Box<dyn Error>>;
 
-pub trait CPUCapLocalIrq: Send + Sync {
-    fn irq_enable(&self, irq: IrqId) -> Result<(), IntcError>;
-    fn irq_disable(&self, irq: IrqId) -> Result<(), IntcError>;
-    fn set_priority(&self, irq: IrqId, priority: usize) -> Result<(), IntcError>;
-    fn set_trigger(&self, irq: IrqId, trigger: Trigger) -> Result<(), IntcError>;
-}
-
-pub trait InterfaceCPU: Send + Sync {
-    fn setup(&self);
-    fn ack(&self) -> Option<IrqId>;
-    fn eoi(&self, irq: IrqId);
-
-    cfg_if! {
-        if #[cfg(target_arch = "aarch64")]{
-            fn dir(&self, intid: IrqId);
-            fn set_eoi_mode(&self, b: bool);
-            fn get_eoi_mode(&self) -> bool;
-        }
-    }
-
-    fn capability(&self) -> CPUCapability;
-}
-
 pub trait Interface: DriverGeneric {
-    fn cpu_interface(&self) -> BoxCPU;
-
     fn irq_enable(&mut self, irq: IrqId) -> Result<(), IntcError>;
     fn irq_disable(&mut self, irq: IrqId) -> Result<(), IntcError>;
     fn set_priority(&mut self, irq: IrqId, priority: usize) -> Result<(), IntcError>;
     fn set_trigger(&mut self, irq: IrqId, trigger: Trigger) -> Result<(), IntcError>;
     fn set_target_cpu(&mut self, irq: IrqId, cpu: CpuId) -> Result<(), IntcError>;
-    fn capabilities(&self) -> Vec<Capability> {
-        Vec::new()
+
+    /// Get CPU local interrupt controller, return None if not supported
+    fn cpu_local(&self) -> Option<local::Boxed>;
+    /// If not supported, returns None
+    fn parse_dtb_fn(&self) -> Option<FuncFdtParseConfig> {
+        None
     }
 }
 
-pub enum Capability {
-    FdtParseConfig(FuncFdtParseConfig),
-}
-
-pub enum CPUCapability {
-    None,
-    LocalIrq(BoxCPUCapLocalIrq),
-}
-
-#[derive(Debug, Clone, Copy)]
+#[derive(thiserror::Error, Debug)]
 pub enum IntcError {
+    #[error("irq `{id:?}` not compatible")]
     IrqIdNotCompatible { id: IrqId },
+    #[error("not support")]
     NotSupport,
+    #[error("other error: {0}")]
+    Other(Box<dyn Error>),
 }
-impl Display for IntcError {
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "{self:?}")
-    }
-}
-impl Error for IntcError {}
