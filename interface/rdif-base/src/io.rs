79f553c453bb1df6c1b5314d5c935710d010ae16
周睿 <zrufo747@outlook.com>
2025-05-29 14:25:14 +0800

    update

--- interface/rdif-base/src/io.rs
+++ interface/rdif-base/src/io.rs
@@ -1,4 +1,4 @@
-use core::{future, task::Poll};
+use core::{fmt::Display, future, task::Poll};
 
 use alloc::boxed::Box;
 pub use async_trait::async_trait;
@@ -100,6 +100,61 @@ pub trait Write {
         })
         .await
     }
+}
+
+pub type Result<T = ()> = core::result::Result<T, Error>;
+
+/// Io error
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Error {
+    /// The kind of error
+    pub kind: ErrorKind,
+    /// The position of the valid data
+    pub success_pos: usize,
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "success pos {}, err:{}", self.success_pos, self.kind)
+    }
+}
+
+impl core::error::Error for Error {}
+
+/// Io error kind
+#[derive(thiserror::Error, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ErrorKind {
+    #[error("Other error: {0}")]
+    Other(&'static str),
+    #[error("Permission denied")]
+    PermissionDenied,
+    #[error("Hardware not available")]
+    NotAvailable,
+    #[error("Broken pipe")]
+    BrokenPipe,
+    #[error("Invalid parameter: {name}")]
+    InvalidParameter { name: &'static str },
+    #[error("Invalid data")]
+    InvalidData,
+    #[error("Timed out")]
+    TimedOut,
+    /// This operation was interrupted.
+    ///
+    /// Interrupted operations can typically be retried.
+    #[error("Interrupted")]
+    Interrupted,
+    /// This operation is unsupported on this platform.
+    ///
+    /// This means that the operation can never succeed.
+    #[error("Unsupported")]
+    Unsupported,
+    /// An operation could not be completed, because it failed
+    /// to allocate enough memory.
+    #[error("Out of memory")]
+    OutOfMemory,
+    /// An attempted write could not write any data.
+    #[error("Write zero")]
+    WriteZero,
 }
 
 #[cfg(test)]
--- interface/rdif-serial/src/lib.rs
+++ interface/rdif-serial/src/lib.rs
@@ -4,27 +4,17 @@ extern crate alloc;
 
 use alloc::boxed::Box;
 
-pub use futures::future::LocalBoxFuture;
-pub use rdif_base::{DriverGeneric, ErrorBase};
-
-pub trait Sender: Send {
-    fn write(&mut self, buf: &[u8]) -> Result<usize, SerialError>;
-    fn write_all<'a>(&'a mut self, buf: &'a [u8]) -> LocalBoxFuture<'a, Result<(), SerialError>>;
-}
-
-pub trait Reciever: Send {
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, SerialError>;
-    fn read_all<'a>(&'a mut self, buf: &'a mut [u8])
-    -> LocalBoxFuture<'a, Result<(), SerialError>>;
-}
+pub use rdif_base::io;
+pub use rdif_base::io::async_trait;
+pub use rdif_base::{DriverGeneric, KError};
 
 pub trait Interface: DriverGeneric {
     /// Call in irq handler.
     fn handle_irq(&mut self);
     /// [`Sender`] will be given back when dropped.
-    fn take_tx(&mut self) -> Option<Box<dyn Sender>>;
+    fn take_tx(&mut self) -> Option<Box<dyn io::Write>>;
     /// [`Reciever`] will be given back when dropped.
-    fn take_rx(&mut self) -> Option<Box<dyn Reciever>>;
+    fn take_rx(&mut self) -> Option<Box<dyn io::Read>>;
 }
 
 /// Serial error kind.
@@ -32,42 +22,39 @@ pub trait Interface: DriverGeneric {
 /// This represents a common set of serial operation errors. HAL implementations are
 /// free to define more specific or additional error types. However, by providing
 /// a mapping to these common serial errors, generic code can still react to them.
-#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+#[derive(thiserror::Error, Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[non_exhaustive]
 pub enum SerialError {
     /// The peripheral receive buffer was overrun.
+    #[error("The peripheral receive buffer was overrun.")]
     Overrun,
     /// Received data does not conform to the peripheral configuration.
     /// Can be caused by a misconfigured device on either end of the serial line.
+    #[error("Received data does not conform to the peripheral configuration.")]
     FrameFormat,
     /// Parity check failed.
+    #[error("Parity check failed.")]
     Parity,
     /// Serial line is too noisy to read valid data.
+    #[error("Serial line is too noisy to read valid data.")]
     Noise,
     /// Device was closed.
+    #[error("Device was closed.")]
     Closed,
     /// A different error occurred. The original error may contain more information.
+    #[error("Unknown error.")]
     Other,
 }
 
-impl core::error::Error for SerialError {}
-
-impl core::fmt::Display for SerialError {
-    #[inline]
-    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        match self {
-            Self::Overrun => write!(f, "The peripheral receive buffer was overrun"),
-            Self::Parity => write!(f, "Parity check failed"),
-            Self::Noise => write!(f, "Serial line is too noisy to read valid data"),
-            Self::FrameFormat => write!(
-                f,
-                "Received data does not conform to the peripheral configuration"
-            ),
-            Self::Closed => write!(f, "Device was closed"),
-            Self::Other => write!(
-                f,
-                "A different error occurred. The original error may contain more information"
-            ),
+impl From<SerialError> for io::ErrorKind {
+    fn from(value: SerialError) -> Self {
+        match value {
+            SerialError::Overrun => io::ErrorKind::BrokenPipe,
+            SerialError::FrameFormat => todo!(),
+            SerialError::Parity => todo!(),
+            SerialError::Noise => todo!(),
+            SerialError::Closed => io::ErrorKind::BrokenPipe,
+            SerialError::Other => todo!(),
         }
     }
 }
